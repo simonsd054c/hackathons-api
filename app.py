716601f6744758e187e0ce812c56f10c7ab7feba
Simon Dahal <simonsd054@gmail.com>
2023-08-12 12:03:37 +1000

    Flask authentication and authorisation

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request #, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
 app = Flask(__name__)
 ma = Marshmallow(app)
 
 # what dbms + db adapter + db_user + password + host:port + database name
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://hackathon_db_admin:password123@localhost:5432/hackathon_db_flask"
+app.config["JWT_SECRET_KEY"] = "secret"
 
 #create the database instance
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 @app.cli.command("create")
 def create_tables():
@@ -17,6 +24,27 @@ def create_tables():
 
 @app.cli.command("seed")
 def seed_tables():
+
+    members = [
+        Member(
+            username = "admin",
+            email = "admin@email.com",
+            password = bcrypt.generate_password_hash("password123").decode('utf-8'),
+            role = "Student",
+            age = 32,
+            is_admin = True
+        ),
+        Member(
+            username = 'user1',
+            email = 'user1@email.com',
+            password = bcrypt.generate_password_hash("password123").decode('utf-8'),
+            role = "Lead Developer",
+            age = 38
+        )
+    ]
+
+    db.session.add_all(members)
+
     project1 = Project(
         title = 'Brisbane Traffic Solver',
         repository = 'https://github.com/traffic_team/traffic_solver', 
@@ -54,6 +82,26 @@ projects_schema = ProjectSchema(many=True)
 project_schema = ProjectSchema()
 
 
+class Member(db.Model):
+    __tablename__ = "members"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    role = db.Column(db.String(), default="Developer")
+    age = db.Column(db.Integer)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class MemberSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "email", "password", "role", "age", "is_admin")
+        load_only = ("password",)
+
+members_schema = MemberSchema(many=True)
+member_schema = MemberSchema()
+
+
 @app.route('/')
 def welcome():
     return "Welcome students to the hackathon!"
@@ -87,6 +135,7 @@ def get_project_by_id(id):
 
 #@app.route('/projects', methods=["POST"])
 @app.post('/projects')
+@jwt_required()
 def create_project():
    #create a project
    #print(request.json)
@@ -101,7 +150,12 @@ def create_project():
    return project_schema.dump(new_project), 201
 
 @app.delete('/projects/<int:id>')
+@jwt_required()
 def delete_project(id):
+
+    if not authorise_as_admin():
+        return {'error': 'Not authorised to delete a project'}, 403
+
     stmt = db.select(Project).filter_by(id = id)
     project = db.session.scalar(stmt)
 
@@ -128,3 +182,47 @@ def update_project(id):
 
     else:
         return {'error': f"Project not found with id {id}"}, 404
+    
+
+@app.post("/auth/register")
+def register_member():
+    member_fields = member_schema.load(request.get_json())
+    password = member_fields.get("password")
+    new_member = Member(
+        username = member_fields.get("username"),
+        email = member_fields.get("email"),
+        password = bcrypt.generate_password_hash(password).decode('utf-8'),
+        role = member_fields.get("role"),
+        age = member_fields.get("age")
+    )
+    db.session.add(new_member)
+    db.session.commit()
+    return member_schema.dump(new_member), 201
+
+@app.post("/auth/login")
+def login_member():
+    # Find a member with that email address
+    stmt = db.select(Member).filter_by(email=request.get_json().get("email"))
+    member = db.session.scalar(stmt)
+    # If member exists and password also matches
+    if member and bcrypt.check_password_hash(member.password, request.get_json().get("password")):
+
+        # create the token and return it
+        token = create_access_token(identity=(str(member.id)), expires_delta=timedelta(days=1))
+        return {'email': member.email, 'token': token, 'is_admin': member.is_admin}
+
+    # else
+    else:
+        # return error message
+        return {'error': 'Invalid email or password'}, 401
+    
+
+def authorise_as_admin():
+    '''
+    return true if the requesting user is an admin
+    return false if the requesting user is not an admin
+    '''
+    member_id = get_jwt_identity()
+    stmt = db.select(Member).filter_by(id=member_id)
+    member = db.session.scalar(stmt)
+    return member.is_admin
